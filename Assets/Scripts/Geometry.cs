0e001db7f572c4ad3ddb094fa4ba56fd0f65da28
redd096 <martin.babino96@gmail.com>
2020-08-13 16:31:27 +0200

    primo script, movimento e camera

---
+++ Assets/Scripts/Geometry.cs
@@ -0,0 +1,269 @@
+﻿namespace redd096
+{
+    using UnityEngine;
+
+    public static class Angle
+    {
+        /// <summary>
+        /// Clamp from min to max
+        /// </summary>
+        public static float ClampAngle(float angle, float min, float max)
+        {
+            //can't go under -360
+            if (angle < -360)
+                angle += 360;
+
+            //can't go over 360
+            if (angle > 360)
+                angle -= 360;
+
+            return Mathf.Clamp(angle, min, max);
+        }
+
+        /// <summary>
+        /// If greater than max, subtract 360 to get negative value
+        /// </summary>
+        public static float NegativeAngle(float angle, float min, float max)
+        {
+            //maybe we need negative value, like -90 instead of 270, for example with clamp from -90 to 90
+            if (angle > max)
+                angle -= 360;
+
+            return Mathf.Clamp(angle, min, max); ;
+        }
+    }
+
+    public static class Quat
+    {
+        /// <summary>
+        /// originalQuat + addedQuat
+        /// </summary>
+        public static Quaternion AddQuaternion(Quaternion originalQuat, Quaternion addedQuat)
+        {
+            return originalQuat * addedQuat;
+        }
+
+        /// <summary>
+        /// originalQuat - subtractedQuat
+        /// </summary>
+        public static Quaternion SubtractQuaternion(Quaternion originalQuat, Quaternion subtractedQuat)
+        {
+            return originalQuat * Quaternion.Inverse(subtractedQuat);
+        }
+    }
+
+    public static class Vect
+    {
+        /// <summary>
+        /// originalVect * multipliedVect
+        /// </summary>
+        public static Vector3 MultiplyVector3(Vector3 originalVect, Vector3 multipliedVect)
+        {
+            Vector3 v;
+
+            v.x = originalVect.x * multipliedVect.x;
+            v.y = originalVect.y * multipliedVect.y;
+            v.z = originalVect.z * multipliedVect.z;
+
+            return v;
+        }
+
+        /// <summary>
+        /// originalVect / dividedVect
+        /// </summary>
+        public static Vector3 DivideVector3(Vector3 originalVect, Vector3 dividedVect)
+        {
+            Vector3 v;
+
+            v.x = originalVect.x / dividedVect.x;
+            v.y = originalVect.y / dividedVect.y;
+            v.z = originalVect.z / dividedVect.z;
+
+            return v;
+        }
+
+        /// <summary>
+        /// originalVect == equalVect (approx is how much difference)
+        /// </summary>
+        public static bool EqualVector3(Vector3 originalVect, Vector3 equalVect, float approx = 0.01f)
+        {
+            //equal of Vector3.Distance(originalVect, equalVect)
+            float distance = (originalVect - equalVect).magnitude;
+
+            bool lowerThanMax = distance < approx;
+            bool greaterThanMax = distance > approx;
+
+            bool equal = lowerThanMax && greaterThanMax;
+
+            return equal;
+        }
+    }
+
+    public static class Math
+    {
+        /// <summary>
+        /// originalFloat == equalFloat (approx is how much difference)
+        /// </summary>
+        public static bool EqualFloat(float originalFloat, float equalFloat, float approx = 0.01f)
+        {
+            bool lowerThanMax = originalFloat < equalFloat + approx;
+            bool greaterThanMax = originalFloat > equalFloat - approx;
+
+            bool equal = lowerThanMax && greaterThanMax;
+
+            return equal;
+        }
+    }
+
+    public static class Direction
+    {
+        /// <summary>
+        /// Return the local direction
+        /// </summary>
+        public static Vector3 WorldToLocalDirection(Vector3 worldDirection, Quaternion rotation)
+        {
+            return rotation * worldDirection;
+        }
+
+        /// <summary>
+        /// Return the world direction
+        /// </summary>
+        public static Vector3 LocalToWorldDirection(Vector3 localDirection)
+        {
+            return Quaternion.identity * localDirection;
+        }
+    }
+
+    public static class TransformRot
+    {
+        /// <summary>
+        /// Transforms rotation from local space to world space. newUp is the up vector of the transform
+        /// </summary>
+        public static Quaternion TransformRotation(Quaternion rotation, Vector3 newUp)
+        {
+            return Quaternion.FromToRotation(Vector3.up, newUp) * rotation;
+        }
+
+        /// <summary>
+        /// Transforms rotation from local space to world space. newUp is the up vector of the transform
+        /// </summary>
+        public static Quaternion TransformRotation(Vector3 rotation, Vector3 newUp)
+        {
+            return Quaternion.FromToRotation(Vector3.up, newUp) * Quaternion.Euler(rotation);
+        }
+
+        /// <summary>
+        /// Transforms rotation from world space to local space. The opposite of TransformRotation.
+        /// </summary>
+        public static Quaternion InverseTransformRotation(Quaternion rotation)
+        {
+            //get local up
+            Vector3 rotationUp = Direction.WorldToLocalDirection(Vector3.up, rotation);
+
+            return Quaternion.FromToRotation(rotationUp, Vector3.up) * rotation;
+        }
+
+        /// <summary>
+        /// Transforms rotation from world space to local space. The opposite of TransformRotation.
+        /// </summary>
+        public static Quaternion InverseTransformRotation(Vector3 rotation)
+        {
+            //get local up
+            Vector3 rotationUp = Direction.WorldToLocalDirection(Vector3.up, Quaternion.Euler(rotation));
+
+            return Quaternion.FromToRotation(rotationUp, Vector3.up) * Quaternion.Euler(rotation);
+        }
+
+        /// <summary>
+        /// Transforms rotation from local space to new transform local space.
+        /// rotation is from the previous transform and newUp is the upVector of the new transform
+        /// </summary>
+        public static Quaternion TransformToTransformRotation(Quaternion rotation, Vector3 newUp)
+        {
+            //get local up
+            Vector3 rotationUp = Direction.WorldToLocalDirection(Vector3.up, rotation);
+
+            return Quaternion.FromToRotation(rotationUp, newUp) * rotation;
+        }
+
+        /// <summary>
+        /// Transforms rotation from local space to new transform local space.
+        /// rotation is from the previous transform and newUp is the upVector of the new transform
+        /// </summary>
+        public static Quaternion TransformToTransformRotation(Vector3 rotation, Vector3 newUp)
+        {
+            //get local up
+            Vector3 rotationUp = Direction.WorldToLocalDirection(Vector3.up, Quaternion.Euler(rotation));
+
+            return Quaternion.FromToRotation(rotationUp, newUp) * Quaternion.Euler(rotation);
+        }
+
+        /// <summary>
+        /// Transforms rotation using local space of another transform, rotated to the local space of another more.
+        /// rotation is what we want. prevUp is the upVector of the first transform, newUp is the upVector of the new transform.
+        /// Used for example for the camera. We want the camera to use the up vector of the player, also when the camera is rotated
+        /// </summary>
+        public static Quaternion OtherTransformToTransformRotation(Quaternion rotation, Vector3 prevUp, Vector3 newUp)
+        {
+            return Quaternion.FromToRotation(prevUp, newUp) * rotation;
+        }
+
+        /// <summary>
+        /// Transforms rotation using local space of another transform, rotated to the local space of another more.
+        /// rotation is what we want. prevUp is the upVector of the first transform, newUp is the upVector of the new transform.
+        /// Used for example for the camera. We want the camera to use the up vector of the player, also when the camera is rotated
+        /// </summary>
+        public static Quaternion OtherTransformToTransformRotation(Vector3 rotation, Vector3 prevUp, Vector3 newUp)
+        {
+            return Quaternion.FromToRotation(prevUp, newUp) * Quaternion.Euler(rotation);
+        }
+    }
+
+    public static class RotatedIdentity
+    {
+        /// <summary>
+        /// Rotate the quaternion.identity to a newUp. Than return new quaternion.identity
+        /// and difference on y axis (MouseX) from default quaternion.identity and the new identity (for RotationWithNewIdentity)
+        /// </summary>
+        public static void RotateQuaternionIdentity(Quaternion currentIdentity, Vector3 newUp, out Quaternion newIdentity, out Quaternion differenceYAxisFromDefaultIdentity)
+        {
+            //to walk on a planet your newUp is player.position - planet.position, or normal using raycast
+
+            //get default quaternion.identity rotated to the new up
+            Quaternion defaultIdentityRotated = TransformRot.TransformRotation(Quaternion.identity, newUp);
+
+            //return new Quaternion.identity
+            newIdentity = TransformRot.TransformToTransformRotation(currentIdentity, newUp);
+
+            //return difference of rotation on Y axis (MouseX), from Quaternion.identity to new identity
+            differenceYAxisFromDefaultIdentity = Quat.SubtractQuaternion(newIdentity, defaultIdentityRotated);
+        }
+
+        /// <summary>
+        /// rotation is the rotation we want but with x = right, y = up and z = forward.
+        /// We want to rotate it from Vector3.up to our new identity up vector.
+        /// We also need difference in y axis from Quaternion.identity to our new identity, because the rotation is applied using z as forward
+        /// </summary>
+        public static Quaternion RotationWithNewIdentity(Vector3 rotation, Vector3 upVector, Quaternion differenceYAxis)
+        {
+            //calculate the rotation with new quaternion.identity (so if our Up vector is not Vector3.up, it works anyway)
+            Quaternion newRotation = TransformRot.TransformRotation(rotation, upVector);
+
+            //there is a problem: Vector3.up has Z as forward, but rotation can have forward rotated, so we must add Y axis (MouseX)
+            return Quat.AddQuaternion(differenceYAxis, newRotation);
+        }
+
+        /// <summary>
+        /// NEVER TESTED
+        /// Reverse RotationWithNewIdentity, so we get the rotation but rotated with x = right, y = up and z = forward
+        /// </summary>
+        public static Quaternion InverseRotationWithNewIdentity(Quaternion rotation, Quaternion differenceYAxis)
+        {
+            //calculate the rotation in world space (so with up vector == Vector3.up)
+            Quaternion newRotation = TransformRot.InverseTransformRotation(rotation);
+
+            //there is a problem: Vector3.up has Z as forward, but rotation can have forward rotated, so we must subtract Y axis (MouseX)
+            return Quat.SubtractQuaternion(newRotation, differenceYAxis);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,63 @@
+﻿using redd096;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] CameraBaseControl cameraControl;
+    [SerializeField] float speed = 4;
+
+    Transform cam;
+    Animator anim;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+        anim = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody>();
+        
+        cameraControl.StartDefault(cam, transform);
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //estrai o riponi arma
+        //attacco
+        //inserire lock sul nemico? va a modificare il CameraBaseControl
+
+        Movement(horizontal, vertical);
+        MoveCamera();
+        Rotate(mouseX, mouseY);
+
+        Animation(horizontal, vertical);
+    }
+
+    void Movement(float horizontal, float vertical)
+    {
+        rb.velocity = Direction.WorldToLocalDirection(new Vector3(horizontal, 0, vertical) * speed, transform.rotation);
+    }
+
+    void MoveCamera()
+    {
+        cameraControl.UpdateCameraPosition();
+    }
+
+    void Rotate(float inputX, float inputY)
+    {
+        cameraControl.UpdateRotation(inputX, inputY);
+    }
+
+    void Animation(float horizontal, float vertical)
+    {
+        anim.SetFloat("Horizontal", horizontal);
+        anim.SetFloat("Vertical", vertical);
+    }
+}

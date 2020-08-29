e161fd41cb1f5b09fa73187a66c52a4fded2aee7
redd096 <martin.babino96@gmail.com>
2020-08-29 17:37:46 +0200

    rinominata cartella Utilities

---
+++ Assets/Scripts/Utilities/UIManager.cs
@@ -0,0 +1,14 @@
+﻿namespace redd096
+{
+    using UnityEngine;
+
+    public class UIManager : MonoBehaviour
+    {
+        [SerializeField] GameObject pauseMenu = default;
+
+        public void PauseMenu(bool active)
+        {
+            pauseMenu.SetActive(active);
+        }
+    }
+}
